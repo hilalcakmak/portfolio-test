a7d69639e4833cad8964dfe52bd276eb3b6843cf
hilalcakmak <cakmak.hilal@hotmail.com>
2024-01-15 16:28:03 +0300

    düzenlendi

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     function handleSubscription(event) {
         event.preventDefault(); 
         alert("Abone oldunuz! Teşekkür ederiz.");
-
-        // Abone olduktan sonra kutu içeriğini temizle
         subscribeForm.reset();
     }
 
@@ -14,25 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //ilk sayfa
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the modals
     var modal = document.getElementById("myModal");
     var spinWheelModal = document.getElementById("spinWheelModal");
 
-    // Display the initial modal when the page is loaded
     modal.style.display = "block";
 
-    // Function to open the spin wheel modal
     window.openSpinWheel = function () {
         modal.style.display = "none";
         spinWheelModal.style.display = "block";
     };
 
-    // Function to close the spin wheel modal
     window.closeSpinWheelModal = function () {
         spinWheelModal.style.display = "none";
     };
 
-    // Close the modals if the user clicks outside of them
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
@@ -61,16 +54,12 @@ function spinWheel() {
 
 //hakkımda
 document.addEventListener('DOMContentLoaded', function () {
-    // İlk div bloğunu seç
     var firstDiv = document.getElementById('main-col');
 
-    // İkinci ve üçüncü div blokları
     var secondDiv = document.querySelector('.about-box');
     var thirdDiv = document.querySelector('.dark');
 
-    // Tıklama olayını ekle
     firstDiv.addEventListener('click', function () {
-        // İkinci ve üçüncü div bloklarını görünür yap
         secondDiv.classList.toggle('hidden');
         thirdDiv.classList.toggle('hidden');
     });
